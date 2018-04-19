624f97be19e8a7cdeeae19942208f4ced7ca01ec
PhamTrung99 <35862709+PhamTrung99@users.noreply.github.com>
2018-04-19 21:20:25 +0700

    Add files via upload

--- 1712848.cpp
+++ 1712848.cpp
@@ -1,54 +1,98 @@
-#include<stdio.h>
+﻿#include<stdio.h>
 #include<string.h>
 #include<malloc.h>
 #include<wchar.h>
 #include<conio.h>
-
-typedef struct sinhvien
+#include <fcntl.h> 
+#include <io.h>    
+#include <codecvt> 
+#include <fstream>
+using namespace std;
+typedef struct SINHVIEN
 {
-	char MSSV[10];
-	wchar_t ten[30];
-	wchar_t khoa[30];
+	wchar_t MSSV[20];
+	wchar_t ten[50];
+	wchar_t khoa[100];
 	int khoatuyen;
-	char ngaysinh[8];
-	char *link_anh;
-	wchar_t mota[1000];
-	wchar_t sothich;
+	wchar_t ngaysinh[50];
+	wchar_t anh[100];
+	wchar_t mota[10000];
+	wchar_t sothich[20][200];
+	int demsothich;
 }sv;
-int demsv(FILE *fi, int &n)
+void tachkhoatuyen(FILE* fi, int &kt)
 {
-	char ch;
-	do
-	{
-		ch = fgetc(fi);
-		if (ch == '\n')
-		{
-			n++;
-		}
-	} while (ch != EOF);
-	return n;
+	fseek(fi,0, SEEK_CUR);
+	fwscanf(fi, L"%d", &kt);
+	fseek(fi,1L, SEEK_CUR);
 }
-void docsv(FILE *fi, FILE *fo, int n)
+void tachsv(FILE* fi, wchar_t nd[])
 {
-	sinhvien a[10];
-	fseek(fi, 0, 0);
-	for (int i = 0; i < n; i++)
+	int i = 0;
+	wchar_t c;
+	fseek(fi,0L, SEEK_CUR);
+	while (1)
 	{
-		fread(a[i].MSSV, sizeof(char), 10, fi);
-		
+		c = fgetwc(fi);
+		if (c == L';' || c == L'\n' || c == EOF) break;
+		else
+		{
+			nd[i] = c;
+			i++;
+		}
 	}
-	fwrite(a[1].MSSV, sizeof(char), 10, fo);
+	nd[i] = '\0';
+}
+int demsothich(FILE*fi)
+{
+	int dem = 0;
+	wchar_t c;
+	while (1)
+	{
+		c = fgetwc(fi);
+		if (c == L';') dem++;
+		if (c == '\n' || c == EOF) break;
+	}
+	return dem  - 6;
+}
+void sothichmoinguoi(FILE* fi, sv sv[10])
+{
+	for (int i = 0; i < 3; i++)
+	{
+		sv[i].demsothich = demsothich(fi);
+	}
+	rewind(fi);
+}
+void luusv(FILE* fi, sv &a){
+	tachsv(fi, a.MSSV);
+	tachsv(fi, a.ten);
+	tachsv(fi, a.khoa);
+	tachkhoatuyen(fi, a.khoatuyen);
+	tachsv(fi, a.ngaysinh);
+	tachsv(fi, a.anh);
+	tachsv(fi, a.mota);
+	/*for (int i = 0; i < a.demsothich; i++)
+	{
+		tachsv(fi, a.sothich[i]);
+	}*/
 }
 void main()
 {
-	FILE *fi = fopen("sinhvien.csv", "r");
-	FILE *fo = fopen("sinhvien.txt", "wt");
-	int n = 0;
+	_setmode(_fileno(stdout), _O_U16TEXT);
+	_setmode(_fileno(stdin), _O_U16TEXT);
+	SINHVIEN a[10];
+	FILE *fi = _wfopen(L"sinhvien.csv", L"r, ccs=UTF-8");
+	int sosv = 2;
 	if (fi != NULL)
 	{
-		int kq = demsv(fi,n);
-		docsv(fi, fo, kq);
+		//sothichmoinguoi(fi, a);
+		fseek(fi,0L,SEEK_SET);
+		for (int i = 0; i < sosv; i++)
+		{
+			luusv(fi, a[i]);
+			wprintf(L"%s\n%s\n%s\n%d\n%s\n%s\n%s\n", a[i].MSSV, a[i].ten, a[i].khoa, a[i].khoatuyen, a[i].ngaysinh, a[i].anh, a[i].mota);
+		}
 	}
-	else printf("Khong doc duoc file.");
-	fcloseall();
+	else wprintf(L"Không đọc được file.");
+	fclose(fi);
 }
